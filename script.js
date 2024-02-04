479d12b745b8dbac22fae806d5461a378ab74d6b
Kanurkar Prateek <53488723+KanurkarPrateek@users.noreply.github.com>
2024-02-04 22:17:15 +0530

    added Javascript code

---
+++ script.js
@@ -0,0 +1,10 @@
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
